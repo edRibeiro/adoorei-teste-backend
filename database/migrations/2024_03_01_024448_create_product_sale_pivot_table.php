5e0b6b3ba65dfde1bb51953358aa2194a6167b9e
edRibeiro <edersonribeiro.ita@gmail.com>
2024-03-01 00:09:48 -0300

    feat: create migration to Sales.

---
+++ database/migrations/2024_03_01_024448_create_product_sale_pivot_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\Product;
+use App\Models\Sale;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_sale', function (Blueprint $table) {
+            $table->unsignedBigInteger('product_id');
+            $table->unsignedBigInteger('sale_id');
+            $table->integer('amount');
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->foreign('sale_id')->references('id')->on('sales');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_sale');
+    }
+};
