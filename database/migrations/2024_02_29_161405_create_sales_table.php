f3e4b32bee6abf7b3c2803f022c61e1195147485
edRibeiro <edersonribeiro.ita@gmail.com>
2024-03-03 03:09:38 -0300

    feat: remove collumn statu from sales

--- database/migrations/2024_02_29_161405_create_sales_table.php
+++ database/migrations/2024_02_29_161405_create_sales_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('sales', function (Blueprint $table) {
             $table->id('id');
             $table->decimal('amount', 8, 2);
-            $table->string('status')->default('pending');
             $table->timestamps();
             $table->softDeletes($column = 'deleted_at', $precision = 0);
         });
