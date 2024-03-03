16fb06280af31542c106e254c8294a76bcd6d785
edRibeiro <edersonribeiro.ita@gmail.com>
2024-03-03 03:23:12 -0300

    feat: Adicionando repository para Sale.

---
+++ app/Repositories/Contracts/SaleRepositoryInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Repositories\Contracts;
+
+use App\Domain\Sale\Sale;
+use App\Dtos\SaleDto;
+
+interface SaleRepositoryInterface
+{
+    public function findAll(): array;
+    public function findById(int $id): Sale|null;
+    public function store(Sale $sale): SaleDto;
+    public function update(Sale $sale): void;
+    public function delete(int $sale_id): void;
+}
---
+++ app/Repositories/SaleEloquentRepository.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Domain\Sale\Sale;
+use App\Dtos\SaleDto;
+use App\Mappers\SaleMapper;
+use App\Models\Sale as SaleModel;
+use App\Repositories\Contracts\SaleRepositoryInterface;
+
+class SaleEloquentRepository implements SaleRepositoryInterface
+{
+    public function findAll(): array
+    {
+        $sales = [];
+        foreach (SaleModel::all() as $saleModel) {
+            $sales[] = SaleDto::fromEloquent($saleModel);
+        }
+        return $sales;
+    }
+
+    public function findById(int $id): Sale
+    {
+        $sale = SaleModel::query()->findOrFail($id);
+        return SaleMapper::fromEloquent($sale);
+    }
+
+    public function store(Sale $sale): SaleDto
+    {
+        $saleEloquent = SaleMapper::toEloquent($sale);
+        $saleEloquent->save();
+        foreach ($sale->products as $product) {
+            $saleEloquent->products()->attach([$product->product_id => ['amount' => $product->amount]]);
+        }
+        return SaleDto::fromEloquent($saleEloquent);
+    }
+
+    public function update(Sale $sale): void
+    {
+    }
+
+    public function delete(int $sale_id): void
+    {
+    }
+}
